ee8e2115d67f38ca7410e58c6aea798cfde90ed8
Jongwon <lastone9182@gmail.com>
2017-02-26 02:56:03 +0900

    Start with Coursera Exploratory Data Analysis

---
+++ cs_eda/week1/R/ExploratoryGraphs.R
@@ -0,0 +1,21 @@
+pollution <- read.csv("../avgpm25.csv", colClasses=c("numeric", "character", "factor", "numeric", "numeric"))
+
+## Five Number Summary
+summary(pollution$pm25)
+
+par(mfrow=c(2, 2))
+
+## Histogram
+hist(pollution$pm25, col="green", breaks=100)
+
+## rug underneath histogram
+rug(pollution$pm25)
+
+## Boxplot
+boxplot(pollution$pm25, col="blue")
+
+## overlaying horizontal abline
+abline(h = 12)
+
+## Barplot
+barplot(table(pollution$region), col = "wheat", main="Number of Counties in Each Region")
